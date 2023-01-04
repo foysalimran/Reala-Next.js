5471da4d9244b0548161bdfe97d665a943d02ed2
sakhawat9 <sakhawathossain7969@gmail.com>
2023-01-04 16:58:24 +0600

    add sass and bootstrap

---
+++ reala-frontend/components/hero.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <div className="hero hero-padding">
+      <div className="container">
+        This is hero
+        <button className="btn btn-primary">button</button>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
--- reala-frontend/pages/index.js
+++ reala-frontend/pages/index.js
@@ -1,13 +1,14 @@
 import Head from "next/head";
 import { Inter } from "@next/font/google";
 import Layout from "../components/global/layout";
+import Hero from "../components/hero";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <Layout>
-      <div>Hello world!</div>
+      <Hero />
     </Layout>
   );
 }
