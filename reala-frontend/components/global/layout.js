dd2710a46b7117fe458e5fc4c21fd4a074db42fc
sakhawat9 <sakhawathossain7969@gmail.com>
2023-01-04 16:40:45 +0600

    initial setup

---
+++ reala-frontend/components/global/layout.js
@@ -0,0 +1,26 @@
+import Head from "next/head";
+import Header from "./Header";
+import Footer from "./Footer";
+
+export default function Layout({ title, keywords, description, children }) {
+  return (
+    <div>
+      <Head>
+        <title>{title}</title>
+        <meta name="description" content={description} />
+        <meta name="keywords" content={keywords} />
+      </Head>
+
+      <Header />
+
+      <div>{children}</div>
+      <Footer />
+    </div>
+  );
+}
+
+Layout.defaultProps = {
+  title: "Reala - Next app",
+  description: "Property Listing NEXT.JS, Strapi app",
+  keywords: "reala, real estate",
+};
