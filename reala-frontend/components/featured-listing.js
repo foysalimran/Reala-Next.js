e41d4b48faae53c6ddb6e8689219f8c11819ae18
sakhawat9 <sakhawathossain7969@gmail.com>
2023-01-11 17:09:42 +0600

    code refactoring

--- reala-frontend/components/featured-listing.js
+++ reala-frontend/components/featured-listing.js
@@ -1,11 +1,7 @@
 import { useState } from "react";
 import { Tab, Tabs } from "react-bootstrap";
 import SectionTitle from "./global/section-title";
-import { AiFillStar, AiOutlineHome } from "react-icons/ai";
-import { GoLocation } from "react-icons/go";
-import { GiBathtub } from "react-icons/gi";
-import { MdBed, MdCall } from "react-icons/md";
-import { API_URL } from "../config";
+import PropertyCard from "./property-card";
 
 const FeaturedListing = ({ data }) => {
   const [key, setKey] = useState("rent");
@@ -32,124 +28,14 @@ const FeaturedListing = ({ data }) => {
             <Tab eventKey="rent" title="Rent">
               <div className="row">
                 {propertyRent.slice(0, 3).map((property) => (
-                  <div key={property.id} className="col-md-6 col-lg-4 mb-4">
-                    <div className="featured-list__item">
-                      <div className="featured-list__item--image">
-                        <img
-                          className="img-fluid"
-                          src={
-                            property?.attributes.image?.data !== null
-                              ? `${API_URL}${property?.attributes.image?.data[0]?.attributes.url}`
-                              : "/images/404.jpg"
-                          }
-                          alt={property.attributes.title}
-                        />
-                        <div className="popular">Popular</div>
-                        <div className="price">$4,550.00 / month</div>
-                      </div>
-                      <div className="featured-list__item__info">
-                        <div className="featured-list__item__info--title">
-                          <h3>
-                            <a href="#">{property.attributes.title}</a>
-                          </h3>
-                        </div>
-                        <div className="featured-list__item__info--ratting">
-                          <span>
-                            <AiFillStar />
-                            {property.attributes.rating}
-                          </span>{" "}
-                          5 reviews
-                        </div>
-                        <ul className="featured-list__item__info--list">
-                          <li>
-                            <span>Office</span>
-                          </li>
-                          <li>
-                            <GoLocation /> {property.attributes.location}
-                          </li>
-                          <li>
-                            <MdCall />{" "}
-                            <a href={`tel${property.attributes.phone}`}>
-                              {property.attributes.phone}
-                            </a>
-                          </li>
-                        </ul>
-                        <ul className="featured-list__item__info--expert">
-                          <li>
-                            <MdBed /> {property.attributes.beds} Beds
-                          </li>
-                          <li>
-                            <GiBathtub /> {property.attributes.ba} Baths
-                          </li>
-                          <li>
-                            <AiOutlineHome /> 6,541 sqft
-                          </li>
-                        </ul>
-                      </div>
-                    </div>
-                  </div>
+                  <PropertyCard property={property} key={property.id} />
                 ))}
               </div>
             </Tab>
             <Tab eventKey="sale" title="Sale">
               <div className="row">
                 {propertySale.slice(0, 3).map((property) => (
-                  <div key={property.id} className="col-md-6 col-lg-4 mb-4">
-                    <div className="featured-list__item">
-                      <div className="featured-list__item--image">
-                        <img
-                          className="img-fluid"
-                          src={
-                            property?.attributes.image?.data !== null
-                              ? `${API_URL}${property?.attributes.image?.data[0]?.attributes.url}`
-                              : "/images/404.jpg"
-                          }
-                          alt={property.attributes.title}
-                        />
-                        <div className="popular">Popular</div>
-                        <div className="price">$4,550.00 / month</div>
-                      </div>
-                      <div className="featured-list__item__info">
-                        <div className="featured-list__item__info--title">
-                          <h3>
-                            <a href="#">{property.attributes.title}</a>
-                          </h3>
-                        </div>
-                        <div className="featured-list__item__info--ratting">
-                          <span>
-                            <AiFillStar />
-                            {property.attributes.rating}
-                          </span>{" "}
-                          5 reviews
-                        </div>
-                        <ul className="featured-list__item__info--list">
-                          <li>
-                            <span>Office</span>
-                          </li>
-                          <li>
-                            <GoLocation /> {property.attributes.location}
-                          </li>
-                          <li>
-                            <MdCall />{" "}
-                            <a href={`tel${property.attributes.phone}`}>
-                              {property.attributes.phone}
-                            </a>
-                          </li>
-                        </ul>
-                        <ul className="featured-list__item__info--expert">
-                          <li>
-                            <MdBed /> {property.attributes.beds} Beds
-                          </li>
-                          <li>
-                            <GiBathtub /> {property.attributes.ba} Baths
-                          </li>
-                          <li>
-                            <AiOutlineHome /> 6,541 sqft
-                          </li>
-                        </ul>
-                      </div>
-                    </div>
-                  </div>
+                  <PropertyCard property={property} key={property.id} />
                 ))}
               </div>
             </Tab>
--- reala-frontend/pages/all-houses.js
+++ reala-frontend/pages/all-houses.js
@@ -1,13 +1,25 @@
 import React from "react";
-import FeaturedListing from "../components/featured-listing";
 import Layout from "../components/global/layout";
+import SectionTitle from "../components/global/section-title";
+import PropertyCard from "../components/property-card";
 import { API_URL } from "../config";
 
 const AllHouses = ({ property }) => {
   const { data } = property;
   return (
     <Layout>
-      <FeaturedListing data={data} />
+      <div className="featured-list section-padding">
+        <div className="container">
+          <SectionTitle title="View All Property" />
+          <div className="featured-listing__wrapper">
+            <div className="row">
+              {data.map((property) => (
+                <PropertyCard property={property} key={property.id} />
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
     </Layout>
   );
 };
